e5bb3527a0baf347fd2876498c5f869e2d028d16
Sondro <noreply@redeclipse.net>
2021-11-07 08:33:50 -0800

    +

---
+++ code/renderer/q_shared.d
@@ -0,0 +1,2 @@
+code/renderer/q_shared.o: code/qcommon/q_shared.c code/qcommon/q_shared.h \
+ code/qcommon/q_platform.h code/qcommon/surfaceflags.h
---
+++ code/renderer2/q_math.d
@@ -0,0 +1,2 @@
+code/renderer2/q_math.o: code/qcommon/q_math.c code/qcommon/q_shared.h \
+ code/qcommon/q_platform.h code/qcommon/surfaceflags.h
